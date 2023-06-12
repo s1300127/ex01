896b4040bf19f232ad080b37092ea35845cc5745
s1300127 <s1300127@u-aizu.ac.jp>
2023-06-12 13:36:37 +0900

    Won/lost message added

--- Dice_Game.c
+++ Dice_Game.c
@@ -13,6 +13,8 @@ int main(){
   printf("Die 1: %d\n",d1);
   printf("Die 2: %d\n",d2);
   printf("Total value: %d\n",d1+d2);
+  if(d1+d2>7)printf("You won.\n");
+  else printf("You lost.\n");
 
   return 0;
 }
