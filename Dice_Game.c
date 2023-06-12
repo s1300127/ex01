a6cf58c45d4a7ccd44282ac5c2647a6fd799bc6c
s1300127 <s1300127@u-aizu.ac.jp>
2023-06-12 13:39:34 +0900

    Won/lost message modified

--- Dice_Game.c
+++ Dice_Game.c
@@ -18,8 +18,8 @@ int main(){
   printf("Die 1: %d\n",d1);
   printf("Die 2: %d\n",d2);
   printf("Total value: %d\n",d1+d2);
-  if(d1+d2>7)printf("You won.\n");
-  else printf("You lost.\n");
+  if(d1+d2>7)printf("%s won.\n",name);
+  else printf("%s lost.\n",name);
 
   return 0;
 }
